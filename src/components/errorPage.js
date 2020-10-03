fd5a70b63f96a2affe2b9f9a3205f9a59da70d39
harshitha tm <37293738+Harshitha-TM@users.noreply.github.com>
2020-10-03 21:28:30 +0530

    Create errorPage.js

---
+++ src/components/errorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+import ErrorIcon from '../errorpuppy.png';
+
+function ErrorPage()  
+{ 
+return ( 
+<div className="errorSection">
+<h4>Sorry, API's are down.</h4> 
+<img src={ErrorIcon}  alt="" width="120px" height="130px"></img>
+</div>
+); 
+} 
+  
+export default ErrorPage;
